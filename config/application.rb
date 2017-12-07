1dc3e545d1da2eefc7abd6ec980e492276a430f7
kotaro.nishi <kotaro.nishi@speee.jp>
2017-12-08 02:26:37 +0900

    set application locale :ja

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,10 @@ Bundler.require(*Rails.groups)
 
 module RandomPhone
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
   end
 end
